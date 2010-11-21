4214758cc9d02bdf3e5db4ec09b53c76ecb2920c
Andre Kelpe <efeshundertelf@googlemail.com>
2010-11-21 18:59:29 +0100

    added usage, cleaned up unused variables, fixed time bug (uses now the current time for the second query) and some other small things

--- tester.py
+++ tester.py
@@ -4,8 +4,7 @@ import httplib
 import urllib
 from xml.dom import minidom
 import sys
-
-BASE_URL =  "http://hari.b-rail.be/Hafas/bin/extxml.exe"
+import time
 
 FIRST = """<?xml version="1.0" encoding="iso-8859-1"?>
 <ReqC ver="1.1" prod="iRail" lang="EN">
@@ -32,7 +31,7 @@ SECOND = """<?xml version="1.0" encoding="iso-8859-1"?>
 </Dest>
 <Via>
 </Via>
-<ReqT time="16:10" date="20100929" a="0">
+<ReqT time="%s" date="%s" a="0">
 </ReqT>
 <RFlags b="0" f="6">
 </RFlags>
@@ -56,27 +55,43 @@ def make_request(xmlstring):
     response = conn.getresponse()
     return response.read()
 
+def now():
+    timestruct = time.localtime()
+    timestamp = "%s:%s" % (timestruct.tm_hour, timestruct.tm_min)
+    day = "%s%s%s" % (timestruct.tm_year, timestruct.tm_mon, timestruct.tm_mday)
+    return timestamp, day
 
-def main():
-    from_station_name = sys.argv[1]
-    to_station_name = sys.argv[2]
-    body = FIRST % (from_station_name, to_station_name)
-    data = make_request(body)
-    doc = minidom.parseString(data)
-
+def find_stations(doc):
     to_station_id, from_station_id = -1, -1
     lvrs = doc.getElementsByTagName("LocValRes")
     for lvr in lvrs:
         st = lvr.getElementsByTagName("Station")[0]
         ext = st.getAttribute("externalId")
-        print ext
+        name = st.getAttribute("name")
+        print "found externalId for station %s: %s" %(name, ext)
         st_type = lvr.getAttribute("id")
         if st_type == "TO":
             to_station_id = ext
         else:
             from_station_id = ext
-    from_station_id, to_station_id
-    body = SECOND % (from_station_id, to_station_id)
+    return from_station_id, to_station_id
+
+
+def main():
+    if len(sys.argv) != 3:
+        print "usage tester.py <from_station_name> <to_station_name>"
+        sys.exit(1)
+    from_station_name = sys.argv[1]
+    to_station_name = sys.argv[2]
+    body = FIRST % (from_station_name, to_station_name)
+    data = make_request(body)
+
+    doc = minidom.parseString(data)
+    print doc.toxml()
+
+    from_station_id, to_station_id = find_stations(doc)
+    tstamp, day = now()
+    body = SECOND % (from_station_id, to_station_id, tstamp, day)
 
     data = make_request(body)
     print data
